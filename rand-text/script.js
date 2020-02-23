bb2801dcb3f17f8ee4e4f40c328fe839ab9f2ddc
TheRiverElder <765375399@qq.com>
2020-02-23 13:16:04 +0800

    Add buttons and fix the README.md

--- rand-text/script.js
+++ rand-text/script.js
@@ -1,13 +1,20 @@
 const iptPattern = document.getElementById('iptPattern');
 const iptKey = document.getElementById('iptKey');
-const spanOutput = document.getElementById('spanOutput');
+const txtOutput = document.getElementById('txtOutput');
+const pnlOutput = document.getElementById('pnlOutput');
 const ovlResult = document.getElementById('ovlResult');
 
-spanOutput.addEventListener('click', event => {
+pnlOutput.addEventListener('click', event => {
     if (event && event.stopPropagation) {
         event.stopPropagation();
     } else {
         event.cancelBubble = true;
+    }
+});
+
+iptKey.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        generate();
     }
 });
 
@@ -22,7 +29,7 @@ function generate() {
         return;
     }
 
-    spanOutput.innerText = '生成中……';
+    txtOutput.innerText = '生成中……';
 
     if (patternChanged) {
         patternManager.clear();
@@ -31,17 +38,17 @@ function generate() {
         patternChanged = false;
     }
 
-    spanOutput.innerText = patternManager.generate(key, {});
-    setResult(true);
+    txtOutput.innerText = patternManager.generate(key, {});
+    setResultVisibility(true);
 }
 
-function setResult(visible) {
+function setResultVisibility(visible) {
     ovlResult.style.display = visible ? 'block' : 'none';
 }
 
 
 function copyPattern() {
-    if (window.navigator.clipboard.writeText) {
+    if (window.navigator.clipboard && window.navigator.clipboard.writeText) {
         window.navigator.clipboard.writeText(iptPattern.value);
     } else {
         iptPattern.select();
@@ -60,5 +67,15 @@ function pastePattern() {
 function clearPattern() {
     if (iptPattern && confirm('确定清空？请对重要数据做好备份，数据会永久消失！')) {
         iptPattern.value = '';
+    }
+}
+
+
+function copyResult() {
+    if (window.navigator.clipboard && window.navigator.clipboard.writeText) {
+        window.navigator.clipboard.writeText(txtOutput.innerText);
+    } else {
+        txtOutput.select();
+        window.document.execCommand('Copy');
     }
 }
